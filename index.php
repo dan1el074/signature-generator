7a7630ed4225a95f53787a4f03049c760e1a0689
Daniel Vargas <daniel.rvargas10@gmail.com>
2024-03-06 09:05:04 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<php
+    echo oi
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerador de Assinaturas</title>
+</head>
+<body>
+    <form method="post">
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name">
+
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name">
+
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name">
+    </form>
+</body>
+</html>
